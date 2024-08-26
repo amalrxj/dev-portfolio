6f57d51a84929047bd8440679c318f8beaa0b9b0
Amal Raj <amalrajkvdev@gmail.com>
2024-08-26 22:56:23 +0530

    spareImg and Correct grid elements

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -11,7 +11,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
     >
@@ -21,21 +21,19 @@ export const BentoGrid = ({
 };
 
 export const BentoGridItem = ({
-  title,
-  id,
-  description,
   className,
+  id,
+  title,
+  description,
   img,
   imgClassName,
-  spareImg,
   titleClassName,
+  spareImg,
 }: {
   className?: string;
+  id?: number;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  icon?: React.ReactNode;
-  id?: number;
   img?: string;
   imgClassName?: string;
   titleClassName?: string;
@@ -53,13 +51,24 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      <div className={`${id===6} && 'flex justify-center h-full`}>
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
-          {img && ( 
-            <img src={img} alt={img} className={cn(imgClassName, 'object-cover, object-center')}
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover object-center")}
             />
           )}
-
+        </div>
+        <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+          {spareImg && (
+            <img
+            src={spareImg}
+            alt={spareImg}
+            className={"object-cover object-center w-full h-full"}
+          />
+          )}
         </div>
       </div>
 
