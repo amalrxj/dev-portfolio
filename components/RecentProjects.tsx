d5d8b05490c531b1f8e3c0ed566e5b8a463afb00
Amal Raj <amalrajkvdev@gmail.com>
2024-08-27 23:01:28 +0530

    git commit before npx resole error

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,5 +1,6 @@
 import { projects } from '@/data'
 import React from 'react'
+import { PinContainer } from './ui/3d-pin'
 
 const RecentProjects = () => {
     return (
@@ -13,7 +14,9 @@ const RecentProjects = () => {
                     id, title, des, img, iconLists, link
                 }) => (
                     <div key={id} className='lg:min-h-[32rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
+                        <PinContainer>
                         {title}
+                        </PinContainer>
                     </div>
                 ))}
 
